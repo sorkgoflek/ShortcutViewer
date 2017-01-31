83089c952ac3ed71de4a80a1e2981723d5f38e26
sorkgoflek <sorkgoflek@naver.com>
2017-01-31 23:00:49 +0900

    System-Hook Lib 동작 확인

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,36 @@
+import lc.kra.system.keyboard.GlobalKeyboardHook;
+import lc.kra.system.keyboard.event.GlobalKeyAdapter;
+import lc.kra.system.keyboard.event.GlobalKeyEvent;
+
 /**
  * Created by Drew on 2017-01-31.
  */
 public class Main {
+    private static boolean run = true;
+
     public static void main(String[] args) {
-        System.out.println("HI");
+        // might throw a UnsatisfiedLinkError if the native library fails to load or a RuntimeException if hooking fails
+        GlobalKeyboardHook keyboardHook = new GlobalKeyboardHook();
+
+        System.out.println("Global keyboard hook successfully started, press [escape] key to shutdown.");
+        keyboardHook.addKeyListener(new GlobalKeyAdapter() {
+            @Override
+            public void keyPressed(GlobalKeyEvent event) {
+                System.out.println(event);
+                if (event.getVirtualKeyCode() == GlobalKeyEvent.VK_ESCAPE)
+                    run = false;
+            }
+
+            @Override
+            public void keyReleased(GlobalKeyEvent event) {
+                System.out.println(event);
+            }
+        });
+
+        try {
+            while (run) Thread.sleep(128);
+        } catch (InterruptedException e) { /* nothing to do here */ } finally {
+            keyboardHook.shutdownHook();
+        }
     }
 }
